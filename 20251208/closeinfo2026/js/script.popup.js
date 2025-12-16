ba846d7638242c3f31874c53e2032638e8f611aa
sekine370 <miyusekine88@outlook.jp>
2025-12-16 11:05:57 +0900

    add

--- 20251208/closeinfo2026/js/script.popup.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($){
-  $(window).on("scroll", function() {
-    if ($(this).scrollTop() > 0) {
-      $('.closeinfo_popup').fadeIn(300); 
-    } else {
-      $('.closeinfo_popup').fadeOut(300);
-    }
-  });
-});
